d6f75f058a9aa1a47d82bed5a7d30de301b48c24
David Aguilera-Riquelme <45923400+davidaguilerar@users.noreply.github.com>
2024-06-29 00:59:16 -0400

    Update redmeteo.py
    Updated to new Redmeteo 3.0 platform

--- bin/user/redmeteo.py
+++ bin/user/redmeteo.py
@@ -1,28 +1,40 @@
-# 2020 by Red Meteoaficionada de Chile.
+# 2020/2024 by Red Meteorologica Aficionada de Chile (Chilean Hobbyist Weather Network).
 # Portions of code from WEEWX-WINDGURU
 
 """
-Esta extension de WeeWX sube datos a Redmeteo.cl.
-
+ENGLISH
+This WeeWX extension uploads data to Redmeteo.cl
 http://redmeteo.cl/
+To join, you have to write an email to redmeteoaficionadachile@gmail.com (ideally in Spanish)
+It's allowed to submit data on 10-minute intervals.
 
+Minimal configuration:
+
+[StdRESTful]
+    [[Redmeteo]]
+        IDrmcl = WEATHERSTATIONID
+        KEYrmcl = STATIONKEY
+
+Redmeteo.cl does not have a well documented API, but sending an email, the Redmeteo team are able to quickly respond your questions.
+
+EXAMPLE: http://redmeteo.cl/subir.php?ID=WEATHERSTATIONID&PASSWORD=STATIONKEY&tempf=outTemp&humidity=RH&dewptf=DewPoint&baromin=Pressure&baromabsin=AbsolutePressure&windspeedmph=WindSpeed&winddir=WindDir&dailyrain=DailyRain&windgustmph=WindGust&solarradiation=radiation&UV=UV&rainrate=rainRate
+
+
+ESPAÑOL
+Esta extension de WeeWX sube datos a Redmeteo.cl.
+http://redmeteo.cl/
 Para registrarte, debes escribir a redmeteoaficionadachile@gmail.com
-
-Se prefiere subir un registro cada 5 minutos (post_interval)
-
+Se admite envios cada 10 minutos (post_interval)
 Configuracion minima:
 
 [StdRESTful]
     [[Redmeteo]]
-        idestacion = ID_USUARIO_REDMETEO.CL
+        IDrmcl = ID_ESTACION_MET_REDMETEO
+        KEYrmcl = CLAVE_ESTACION
 
 Redmeteo.cl no tiene una API documentada pero escribiendo al correo,
  pueden responder rapidamente con las dudas
-
-Los datos se envian en una Solicitud GET de este tipo:
-
-http://redmeteo.cl/extelemetry.php?format=raw&ID=ID_USUARIO_REDMETEO.CL&date=DD/MM/YY&time=12:34&temp=outTemp&heatindex=heatIndex&windchill=windChill&rh=RH&dewpoint=DewPoint&baro=Pressure&prestrend=PressTrend&wsavg=WindSpeed&wdirangle=WindDir&rain=DailyRain&wgust=WindGust&wsunit=WindUnit&tempunit=TempUnit&presunit=PressureUnit&rainunit=RainUnit
-
+ 
 """
 
 try:
@@ -48,7 +60,7 @@ import weewx.units
 VERSION = "0.1"
 
 if weewx.__version__ < "3":
-    raise weewx.UnsupportedFeature("WeeWX 3 es requerido, usted tiene %s" %
+    raise weewx.UnsupportedFeature("WeeWX 3 is required, you have %s" %
                                    weewx.__version__)
 
 
@@ -91,12 +103,13 @@ class Redmeteo(weewx.restx.StdRESTbase):
     def __init__(self, engine, config_dict):
         """Este servicio reconoce el siguiente login
 
-        idestacion: ID de usuario o estacion en Redmeteo.cl
+        idestacion: ID de usuario o estacion en Redmeteo.cl / 
+        clave: Clave de la estación en Redmeteo.cl / Station Key
 
         """
         super(Redmeteo, self).__init__(engine, config_dict)
         loginf("service version is %s" % VERSION)
-        site_dict = weewx.restx.get_site_dict(config_dict, 'Redmeteo', 'idestacion')
+        site_dict = weewx.restx.get_site_dict(config_dict, 'Redmeteo', 'idestacion', 'claveestacion')
         if site_dict is None:
             return
 
@@ -117,27 +130,29 @@ class Redmeteo(weewx.restx.StdRESTbase):
 
 
 class RedmeteoThread(weewx.restx.RESTThread):
-    _SERVER_URL = 'https://redmeteo.cl/extelemetry.php'
-    _DATA_MAP = {'temp': ('outTemp', '%.1f'),  # C
-                 'wdirangle': ('windDir', '%.0f'),  # degree
-                 'wsavg': ('windSpeed', '%.1f'),  # knots
-                 'wgust': ('windGust', '%.1f'),  # knots
-                 'baro': ('barometer', '%.3f'),  # hPa
-                 'rh': ('outHumidity', '%.1f'),  # %
-                 'rain': (('dayRain'), '%.2f'),  # mm
-                 'heatindex': ('heatindex', '%.1f'), # ?
-                 'windchill': ('windchill', '%.1f'), # C
-                 'dewpoint': ('dewpoint', '%.1f'), # C
-                 'presstrend': "NULL",   #nothing
-                 'uv': ('UV', '%.0f'), # index
-                 'sw': ('radiation', '%.0f') # W/m^2
+    _SERVER_URL = 'https://redmeteo.cl/subir.php'
+    _DATA_MAP = {'tempf': ('outTemp', '%.1f'),              # F
+                 'winddir': ('windDir', '%.0f'),            # degree
+                 'windspeedmph': ('windSpeed', '%.1f'),     # mph
+                 'windgustmph': ('windGust', '%.1f'),       # mph
+                 'baromin': ('barometer', '%.3f'),          # inHg
+                 'baromabsin': ('pressure', '%.3f'),        # inHg
+                 'humidity': ('outHumidity', '%.0f'),       # %
+                 'dailyrainin': (('dayRain'), '%.3f'),      # in
+                 'monthlyrainin': (('monthRain'), '%.3f'),  # in
+                 'yearlyrainin': (('yearRain'), '%.3f'),    # in
+                 'rainin': (('hourRain'), '%.3f'),          # in
+                 'rainratein': (('rainRate'), '%.3f'),      # in/h
+                 'dewptf': ('dewpoint', '%.1f'),            # F
+                 'UV': ('uv', '%.0f'),                      # index (dimensionless)
+                 'solarradiation': ('radiation', '%.0f')    # W/m^2
                  }
 
     def __init__(self, queue, idestacion, manager_dict,
                  server_url=_SERVER_URL, skip_upload=False,
-                 post_interval=60, max_backlog=sys.maxsize, stale=None,
+                 post_interval=600, max_backlog=sys.maxsize, stale=None,
                  log_success=True, log_failure=True,
-                 timeout=60, max_tries=3, retry_wait=5):
+                 timeout=600, max_tries=2, retry_wait=5):
         super(RedmeteoThread, self).__init__(queue,
                                              protocol_name='Redmeteo',
                                              manager_dict=manager_dict,
@@ -151,6 +166,7 @@ class RedmeteoThread(weewx.restx.RESTThread):
                                              retry_wait=retry_wait,
                                              skip_upload=skip_upload)
         self.idestacion = idestacion
+        self.claveestacion = claveestacion
         self.server_url = server_url
 
     def check_response(self, response):
@@ -163,7 +179,7 @@ class RedmeteoThread(weewx.restx.RESTThread):
 
     def format_url(self, in_record):
         # put everything into the right units and scaling
-        record = weewx.units.to_METRICWX(in_record)
+        record = weewx.units.to_US(in_record)
         if 'windSpeed' in record and record['windSpeed'] is not None:
             record['windSpeed'] = _mps_to_knot(record['windSpeed'])
         if 'windGust' in record and record['windGust'] is not None:
@@ -172,14 +188,8 @@ class RedmeteoThread(weewx.restx.RESTThread):
         # put data into expected structure and format
         time_tt = time.localtime(record['dateTime'])
         values = {
-            'format': 'stickertag',
             'ID': self.idestacion,
-            'date': time.strftime("%d/%m/%Y", time_tt),
-            'time': time.strftime("%H:%M:%S", time_tt),
-            'wsunit': "kts",
-            'tempunit': "C",
-            'presunit': "hPa",
-            'rainunit': "mm"
+            'PASSWORD': self.claveestacion
         }
 
         for key in self._DATA_MAP:
